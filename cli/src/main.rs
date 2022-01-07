22d148eb31a416455d3d4c3be592d3a97d95b331
Kent Utterback <Kent.Utterback@cunamutual.com>
2022-01-07 13:52:23 -0800

    start cli

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,17 @@
+use clap::{Parser};
+
+#[derive(Parser, Debug)]
+#[clap(about, author, version)]
+struct Args {
+    #[clap(short, long)]
+    name: String,
+    #[clap(short, long, default_value_t = 1)]
+    count: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    for _ in 0..args.count {
+        println!("Hello, {}!", args.name);
+    }
 }
